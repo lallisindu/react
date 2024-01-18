cf266e76dbd27e93e3318521d15d56d22a6a2e03
lallisindu <lallisindu123@gmail.com>
2024-01-18 22:33:03 +0530

    creating props passing values

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 import ExpenciveItem from './components/ExItem.js';
-function App(){
-  return (<div><h1>Lets start</h1>
-          <ExpenciveItem></ExpenciveItem></div>);
-    
-  
-  
 
-};
+const expenses = [
+  {
+    id: 1,
+    title: 'paper',
+    amount: 50,
+    date: new Date(2024, 1, 18),
+    location: 'India',
+  },
+  {
+    id: 2,
+    title: 'TV',
+    amount: 50000,
+    date: new Date(2024, 1, 18),
+    location: 'India',
+  },
+  // Add more expenses
+];
+
+function App() {
+  return (
+    <div>
+      <h1>Lets start</h1>
+      {expenses.map((expense) => (
+        <ExpenciveItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
+    </div>
+  );
+}
+
 export default App;
-
-
--- src/components/ExItem.js
+++ src/components/ExItem.js
@@ -1,4 +1,10 @@
-function ExpenciveItem(){
-    return <div><h2> ExpensiveItem</h2><div> Food RS 10</div><div> petrol RS 10</div><div> movies RS 10</div></div>
+import './ExItem.css'
+function ExpenciveItem(props){
+    
+    return <div className='expense-item '>
+        <h2>{props.date.toISOString}</h2 >
+        <div className='expense-item__description'><h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+        </div><div className='expense-item__location'>{props.location}</div></div>
 }
 export default ExpenciveItem;
