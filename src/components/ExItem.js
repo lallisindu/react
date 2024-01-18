e85a753e9b825d44d6dbca827b91318541fbf26a
lallisindu <lallisindu123@gmail.com>
2024-01-18 23:57:14 +0530

    arrow functions

--- src/components/ExItem.js
+++ src/components/ExItem.js
@@ -5,6 +5,11 @@ function ExpenciveItem(props){
         <h2>{props.date.toISOString}</h2 >
         <div className='expense-item__description'><h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
-        </div><div className='expense-item__location'>{props.location}</div></div>
+        </div><div className='expense-item__location'>{props.location}</div>
+        
+      <div>
+        title={props.title}
+        amount={props.amount}
+        location={props.location}</div></div>
 }
 export default ExpenciveItem;
--- src/components/ExpenciveItem.js
+++ src/components/ExpenciveItem.js
@@ -1,4 +1,4 @@
-function ExpenciveItem(){
+const ExpenciveItem=() =>{
     return <h2> ExpensiveItem</h2>
 }
 export default ExpenciveItem;
