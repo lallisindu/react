759e90da94854b28a32181aec4a063a3656a03c4
lallisindu <lallisindu123@gmail.com>
2024-01-22 13:30:24 +0530

    stateless componets

--- src/ExItem.js
+++ src/ExItem.js
@@ -1,15 +1,11 @@
-import React,{useState} from "react";
+import React from "react";
 import ExpenseDate from "./ExDate";
 import Card from "./Card";
 function ExpenseItem(props){
-    const [title,setTitle]=useState(props.title);
-    const ClickHandler=() =>{
-       setTitle('Updated!')
-       console.log('Expensive Iitem evaluated by React!')
-    }
+    
    
     return (
-        <Card><div>{title}</div>
+        <Card><div>{props.title}</div>
               <div>{props.amount}</div> 
               <div><ExpenseDate date={props.date}/></div> 
               <button onClick={ClickHandler}>Change title</button></Card>
