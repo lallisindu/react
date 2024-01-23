66940f7dac46d73af36a026c1a60ba7ab621ee0b
lallisindu <lallisindu123@gmail.com>
2024-01-23 11:34:47 +0530

    adding barchart

--- src/Expenses.js
+++ src/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from './ExpenseItem';
 import Card from './Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -56,6 +57,7 @@ const Expenses = (props) => {
           />
         ))}
         {expensesContent}
+        <Expenses expense={filteredExpenses}/>
       </Card>
     </div>
   );
